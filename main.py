3ec7c47569930f5454e665d826958ab259df5973
ninohantonio <ninohantonio@gmail.com>
2024-09-10 11:30:03 +0200

    update main launcher

--- main.py
+++ main.py
@@ -5,19 +5,6 @@ from PySide6.QtWidgets import QApplication
 from models.model_class import Client
 from views.main_ui_launcher import MainWindow
 
-
-def print_article(client):
-    # for article in articles:
-    print(f"""
-                id = {client.numeroClient}
-                nom = {client.nom}
-                adresse = {client.adresse}
-                telephone = {client.telephone}
-           """)
-
-client1 = Client(nom = "RAKOTO be", adresse = "lot 1234 uidfadsidaf tanambao", telephone="0341234567")
-client2 = Client(nom = "RANDRIA kely", adresse = "lot 1234 uidfadsidaf tanambao", telephone="0343214561")
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app = QApplication(sys.argv)
