eea23ead5f46365bed028fbda1185ddf8e1f6953
ninohantonio <ninohantonio@gmail.com>
2024-09-13 08:28:49 +0200

    make article data controller

---
+++ controllers/article_controller.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from PIL.ImImagePlugin import split
+
+from models.model_class import Article
+
+
+def insert_article_controller(article: Article) -> str:
+    if article.prixUnitaire < 0 :
+        return "prix"
+    elif article.quantiteStock < 0:
+        return "quantite"
+    else:
+        return ""
+
+def get_date_to_string() -> str:
+    date = datetime.now()
+    date = str(date)
+    date = date.split(' ')
+    return date[0]
+
+def get_date_time_to_string() -> str:
+    date = datetime.now()
+    date = str(date)
+    date = date.split('.')
+    return date[0]
