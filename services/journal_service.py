6a86339f3768feb1fd7325bb6ae2e3970fb69610
ninohantonio <ninohantonio@gmail.com>
2024-10-08 10:15:41 +0200

    make journal service

---
+++ services/journal_service.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import sessionmaker
+
+from models.model_class import Journal
+from utils.database import engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def get_journal_by_type_action(action: str):
+    return session.query(Journal).filter(Journal.typeAction.contains(action)).all()
+
+def insert_new_journal(journal: Journal):
+    session.add(journal)
+    session.commit()
