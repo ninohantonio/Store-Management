2dac790bb6c7c0ea1342b8a02b8d979dd95c658f
ninohantonio <ninohantonio@gmail.com>
2024-10-15 11:25:26 +0200

    approvisionnement services

---
+++ services/approvisionnement_service.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import sessionmaker
+
+from models.model_class import Approvisionnement
+from services.article_service import get_article_by_id
+from utils.database import engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def add_new_approvisionnement(appro: Approvisionnement):
+    session.add(appro)
+    session.commit()
+
+def get_all_approvisionnement():
+    return session.query(Approvisionnement).all()
+
+def verify_if_approvisionnement_low(appro: Approvisionnement):
+    article = get_article_by_id(appro.numeroArticle)
+    if appro.typeQuantite == "Paquet":
+        return appro.quantiteLimite >= article.packetEnStock
+    elif appro.typeQuantite == "Boite":
+        return appro.quantiteLimite >= article.boiteEnStock
+    else:
+        return appro.quantiteLimite >= article.pieceEnStock
+
+def get_article_in_limite() -> list[str]:
+    articles = []
+    for appro in get_all_approvisionnement():
+        if verify_if_approvisionnement_low(appro):
+            articles.append(appro.numeroArticle)
+
+    return articles
